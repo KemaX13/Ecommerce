5fa6f46bfd2bca193149fb741c00e16f08965637
erick <cantosfabricio529@gmail.com>
2024-10-25 10:55:18 +0200

    slider creado con react-slick y imagenes añadidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import SimpleSlider from "./components/slider.jsx"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="container">
+
+
+    <SimpleSlider></SimpleSlider>
+
+
+
+    </div>
   )
 }
 
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import "../styles/slider.css"
 
-function slider() {
+import aPlagueTaleRequiem from "../assets/imgs/aPlagueTaleRequiem.jpg"
+import diabloIV from "../assets/imgs/diabloIV.jpg"
+import fallGuys from "../assets/imgs/fallguys.jpg"
+import gtaVI from "../assets/imgs/grandTheftAutoVI.jpg"
+import kofXV from "../assets/imgs/kofXV.jpg"
+import marioKartDeluxe8 from "../assets/imgs/marioKartDeluxe8.jpg"
+
+const SimpleSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    centerMode: true,
+    centerPadding: "0",
+  };
+
   return (
-    <div>slider</div>
-  )
-}
+    <Slider {...settings}>
+      <div>
+        <img src={aPlagueTaleRequiem} alt="" />
+      </div>
+      <div>
+        <img src={diabloIV} alt="" />
+      </div>
+      <div>
+        <img src={fallGuys} alt="" />
+      </div>
+      <div>
+        <img src={gtaVI} alt="" />
+      </div>
+      <div>
+        <img src={kofXV} alt="" />
+      </div>
+      <div>
+        <img src={marioKartDeluxe8} alt="" />
+      </div>
+    </Slider>
+  );
+};
 
-export default slider
+export default SimpleSlider;
