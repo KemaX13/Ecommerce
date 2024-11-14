abbad2c32a2fc13a3e71cf0753482d06576094b8
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2024-11-14 09:15:01 +0100

    Add imgs

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import TwoColumns from './Components/TwoColumns/TwoColumns'
 import Links from './Components/Links/Links'
 
 function App() {
-  const gitHub = <Links imgPath='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png'
-    altImg='GitHub'
-    url='https://github.com/'
-    text='GitHub'
-    param='both' />
+  const logoIcon = <Links url='/'
+    text='Logo'
+    param='text'/>
+
+  const userIcon = <Links imgPath='./img/user.png'
+    altImg='UserIcon'
+    param='img'/>
+
+  const cartIcon = <Links imgPath='./img/cart.png'
+    altImg='CartIcon'
+    param='img'/>
+
+  const rElemH = [userIcon, cartIcon]
 
   return (
-    <header className='header twoColumns'><TwoColumns leftElmts={gitHub} rightElmts={null} /></header>
+    <header className='header twoColumns'><TwoColumns leftElmts={ logoIcon } rightElmts={ rElemH } /></header>
   )
 }
 
